755384ec31d99d808d97dd1c7c2fec021b344d26
Costi <costi_georgescu2000@yahho.com>
2022-12-03 13:23:21 +0200

    Initial commit

---
+++ src/main/java/com/george/expensetrackerapi/controller/ExpenseController.java
@@ -0,0 +1,52 @@
+package com.george.expensetrackerapi.controller;
+
+import com.george.expensetrackerapi.entity.Expense;
+import com.george.expensetrackerapi.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @GetMapping("/expenses")
+    public List<Expense> getExpenses(Pageable page) {
+        int number = 1;
+        calculateFactorial(number);
+        return expenseService.getAllExpenses(page).toList();
+    }
+
+    @GetMapping("expenses/{id}")
+    public Expense getExpenseById(@PathVariable Long id) {
+        return expenseService.getExpenseById(id);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/expenses")
+    public void deleteExpenseById(@RequestParam Long id) {
+        expenseService.deleteExpenseById(id);
+    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping("/expenses")
+    public Expense saveExpenseDetails(@RequestBody Expense expense) {
+        return expenseService.saveExpenseDetails(expense);
+    }
+
+
+    @PutMapping("/expenses/{id}")
+    public Expense updateExpenseDetails(@RequestBody Expense expense, @PathVariable Long id) {
+        return expenseService.updateExpenseDetails(id, expense);
+    }
+
+    public int calculateFactorial(int number) {
+        return number * calculateFactorial(number - 1);
+    }
+}
---
+++ src/main/java/com/george/expensetrackerapi/service/ExpenseServiceImpl.java
@@ -0,0 +1,53 @@
+package com.george.expensetrackerapi.service;
+
+import com.george.expensetrackerapi.entity.Expense;
+import com.george.expensetrackerapi.exception.ResourceNotFoundException;
+import com.george.expensetrackerapi.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ExpenseServiceImpl implements ExpenseService {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @Override
+    public Page<Expense> getAllExpenses(Pageable page) {
+        return expenseRepository.findAll(page);
+    }
+
+    @Override
+    public Expense getExpenseById(Long id) {
+        Optional<Expense> expense = expenseRepository.findById(id);
+        if (expense.isPresent()) {
+            return expense.get();
+        }
+        throw new ResourceNotFoundException("The expense with id " + id + " is not present");
+    }
+
+    @Override
+    public void deleteExpenseById(Long id) {
+        expenseRepository.deleteById(id);
+    }
+
+    @Override
+    public Expense saveExpenseDetails(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    @Override
+    public Expense updateExpenseDetails(Long id, Expense expense) {
+        Expense existingExpense = getExpenseById(id);
+        existingExpense.setName(expense.getName() != null ? expense.getName() : existingExpense.getName());
+        existingExpense.setDescription(expense.getDescription() != null ? expense.getDescription() : existingExpense.getDescription());
+        existingExpense.setAmount(expense.getAmount() != null ? expense.getAmount() : existingExpense.getAmount());
+        existingExpense.setCategory(expense.getCategory() != null ? expense.getCategory() : existingExpense.getCategory());
+        existingExpense.setDate(expense.getDate() != null ? expense.getDate() : existingExpense.getDate());
+        return expenseRepository.save(existingExpense);
+    }
+}
